0eafabf8f8f700dc8879acb87515bb7874080635
Serjucho-PC <sergilivo22@gmail.com>
2023-03-31 16:04:44 +0200

    new commit

---
+++ M05_UF2_P2_ProjectFix-master/DNI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DNI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your DNI/NIF number (Without the letter):");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your complete DNI/NIF is: " + number + LetterNIF(number));
+        }
+
+        /// <summary>
+        /// This 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static char LetterNIF(int number)
+        {
+            string LetterNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int resto = number % 23;
+            return LetterNIF[resto];
+        }
+    }
+}
---
+++ M05_UF2_P2_ProjectFix-master/Sorting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unordered list: ");
+            List<int> list = new List<int>(100);
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(Utils.random.Next());
+                Console.WriteLine(list[i]);
+            }
+
+            list = Sort(list);
+            Console.WriteLine("Ordered list: ");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+        static public List<int> Sort(List<int> list)
+        {
+            list.Sort();
+            return list;
+        }
+    }
+}
